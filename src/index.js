312990dbc11db79f8c39b7d7becbb6f790269cdd
Akhand Raj Singh <akhandrajsingh30@gmail.com>
2025-06-03 15:25:18 +0530

    Utility code Discussion

--- src/index.js
+++ src/index.js
@@ -1,20 +1,28 @@
 // require('dotenv').config({path:'./env'})
 
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constants";
 import connectDB from "./db/index.js";
 
 dotenv.config({
-    path:'./env'
-})
+  path: "./env",
+});
 
 connectDB()
-
-
-
-
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("Errrrr", error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server is running at port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("mongoDB connection feild : ", err);
+  });
 
 /*
 
---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,8 @@
+class ApiResponse {
+    constructor(statusCode,data,message = "success"){
+        this.statusCode = starcode
+        this.data = data
+        this.data = message
+        this.sucess = statuscode < 400
+    }
+}
