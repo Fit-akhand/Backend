066c3eef2272ee3f9641b1aca769ab17c860eb0f
Akhand Raj Singh <akhandrajsingh30@gmail.com>
2025-08-10 17:12:26 +0530

    file uplode using cloudinary and multer

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,31 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/temp");
+  },
+  filename: function (req, file, cb) {
+    // cb(null, file.fieldname + "-" + uniqueSuffix);
+    cb(null, file.originalname);
+    /*
+    {
+    console.log(file)
+  fieldname: 'avatar',                 // Name of the form field
+  originalname: 'mypic.jpg',           // Original name of the file from the client
+  encoding: '7bit',                    // File encoding type
+  mimetype: 'image/jpeg',              // MIME type (image/png, image/jpeg, etc.)
+  destination: './uploads/',           // Folder where file is saved
+  filename: 'avatar-1723287312345-456789123.jpg', // New name assigned by Multer
+  path: 'uploads/avatar-1723287312345-456789123.jpg', // Full path to saved file
+  size: 48291                          // File size in bytes
+}
+    */
+  },
+});
+
+export const upload = multer({
+  //  storage: storage
+
+  //ES6
+  storage,
+});
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,33 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  // Click 'View API Keys' above to copy your API secret
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+
+const uplodeonCloudinary = async (localfilepath) => {
+    try {
+        if(!localfilepath) return null
+        // uplode the file on cloudinary
+        const response = await cloudinary.uploader.upload(localfilepath,{
+            resource_type : "auto"
+        })
+        //file has been uploded successfully
+        console.log("file is uplodede on cloudinary",response.url); 
+        //  response URL in your website or app to display the image/video without storing it locally.
+        return response; 
+
+    } catch (error) {
+        fs.unlinkSync(localfilepath)  // remove the localy saved temporary file as the uplode operation got failed
+
+        return null;
+    }
+}
+
+
+
+export {uplodeonCloudinary}
