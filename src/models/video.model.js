192cc7e03f9ecb701e5ebbecb8bb7ad127d47d85
Akhand Raj Singh <akhandrajsingh30@gmail.com>
2025-08-10 17:50:34 +0530

    video model

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,46 +1,47 @@
-import mongoose,{mongo, Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
 const videoSchema = new Schema(
-    {
-        videoFile:{
-            type: String,  //cloudinary url
-            required:true,
-        },
-        thumbnail:{        //cloudinary url
-            type:String,
-            required:true
-        },
-        owner:{
-            type:Schema.Types.ObjectId,
-            ref:"User"
-        },
-        title:{
-            type:String,
-            required:true
-        },
-        description:{
-            type:String,
-            required:true,
-        },
-        duration:{
-            type:Number,   //cloudinary url
-            required:true
-        },
-        views:{
-            type:Number, 
-            default:0
-
-        },
-        isPublished:{
-            type:Boolean,
-             default: true
-        }
+  {
+    videoFile: {
+      type: String, //cloudinary url
+      required: true,
     },
-    {
-        timestamps:true
-    }
-)
+    thumbnail: {
+      //cloudinary url
+      type: String,
+      required: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: Number, //cloudinary url
+      required: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-videoSchema.plugin(mongooseAggregatePaginate)
- 
-export const Video = mongoose.model("Video",videoSchema)
+videoSchema.plugin(mongooseAggregatePaginate);
+
+export const Video = mongoose.model("Video", videoSchema);
