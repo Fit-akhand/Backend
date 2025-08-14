f3ca3db4f96fd8dd3c2aa0176d47888dcef2ca47
Akhand Raj Singh <akhandrajsingh30@gmail.com>
2025-08-14 23:45:08 +0530

    get user detail

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ app.use(cookieParser())
 
 // routes import 
 
-import userRouter from '../routes/user.routes.js'
+import userRouter from './routes/user.routes.js'
 
 //routes declaration
 app.use("/api/v1/user",userRouter)
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,31 @@
 import {asyncHandler} from "../utils/asyncHandler.js";
-
+import {ApiError} from "../utils/ApiError.js"
 const registerUser = asyncHandler( async (req,res) => {
-    res.status(200).json({
-        message : "ok"
-    })
+    // get user detail from frontend
+    //validation - not empty 
+    //check if user already exist : user email
+    //check for images , check for avatar
+    //upload them to cloudinary , avatar
+    //create user object - create entry in db
+    //remove password and refresh token field from response
+    //check for user creation
+    //return res
+
+    const {fullname,email,username,password} = req.body
+    console.log("email",email)
+
+    //you can handle one by one 
+    // if(fullname === ""){
+    //     throw new ApiError(400,"full name is required")
+    // }
+
+    if (
+        [fullname,email,username,password].some((field) => field?.trim() === "")
+    ) {
+        
+    }
+    
+
 })
 
 
