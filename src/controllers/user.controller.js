d56c5681142baccb640bf47bd6b3512394ca6489
Akhand Raj Singh <akhandrajsingh30@gmail.com>
2025-08-19 23:08:43 +0530

    controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,32 +1,28 @@
-import {asyncHandler} from "../utils/asyncHandler.js";
-import {ApiError} from "../utils/ApiError.js"
-const registerUser = asyncHandler( async (req,res) => {
-    // get user detail from frontend
-    //validation - not empty 
-    //check if user already exist : user email
-    //check for images , check for avatar
-    //upload them to cloudinary , avatar
-    //create user object - create entry in db
-    //remove password and refresh token field from response
-    //check for user creation
-    //return res
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+const registerUser = asyncHandler(async (req, res) => {
+  // get user detail from frontend
+  //validation - not empty
+  //check if user already exist : user email
+  //check for images , check for avatar
+  //upload them to cloudinary , avatar
+  //create user object - create entry in db
+  //remove password and refresh token field from response
+  //check for user creation
+  //return res
 
-    const {fullname,email,username,password} = req.body
-    console.log("email",email)
+  const { fullname, email, username, password } = req.body;
+  console.log("email", email);
 
-    //you can handle one by one 
-    // if(fullname === ""){
-    //     throw new ApiError(400,"full name is required")
-    // }
+  //you can handle one by one
+  // if(fullname === ""){
+  //     throw new ApiError(400,"full name is required")
+  // }
 
-    if (
-        [fullname,email,username,password].some((field) => field?.trim() === "")
-    ) {
-        
-    }
-    
+  if (
+    [fullname, email, username, password].some((field) => field?.trim() === "")
+  ) {
+  }
+});
 
-})
-
-
-export {registerUser}
+export { registerUser };
