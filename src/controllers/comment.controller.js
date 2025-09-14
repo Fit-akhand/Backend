e2381e64c8bf0f0a8d652bb8fe9b22b36f341a7b
Akhand Raj Singh <akhandrajsingh30@gmail.com>
2025-09-15 01:07:21 +0530

    feat: implement video comments retrieval with input validation and error handling

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -3,11 +3,34 @@ import {Comment} from "../models/comment.model.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
+import { Video } from "../models/video.model.js"
 
 const getVideoComments = asyncHandler(async (req, res) => {
-    //TODO: get all comments for a video
+
+    //Get Inputs from Request
     const {videoId} = req.params
-    const {page = 1, limit = 10} = req.query
+    const {page = 1, limit = 10} = req.query   // page one and the no of comments should be not more than 10
+
+    // Validate Inputs
+    if(!videoId){
+        throw new ApiError(404,"Video is invalid")
+    }
+
+    //Check if video exists
+    const video = await Video.findById(videoId);
+
+    if(!video){
+        throw new ApiError(404,"Video not found")
+    }
+
+    //Fetch comments
+    
+
+
+
+
+    
+
 
 })
 
